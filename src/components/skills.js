34e142207bebb7f14bba1207cb1cffe778e7bd41
goughjo02 <goughjo02@gmail.com>
2018-11-11 13:47:22 +0000

    changed rendering logic of skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -23,145 +23,121 @@ const styles = theme => ({
 });
 
 function SkillsSection(props) {
-    const webTechnologies = [
+    const skills = [
         {
-            title: "HTML5 / CSS3 / Javascript"
+            title: "Web Technologies",
+            skills: [
+                {
+                    title: "HTML5 / CSS3 / Javascript"
+                },
+                {
+                    title: "ReactJS / Redux"
+                },
+                {
+                    title: "Angular 7"
+                },
+                {
+                    title: "Websockets / RESTful APIs"
+                },
+                {
+                    title: "Data Visualisation / D3"
+                },
+                {
+                    title: "C# / .NET"
+                },
+                {
+                    title: "Python Flask / Django"
+                },
+                {
+                    title: "linux"
+                },
+                {
+                    title: "NGINX"
+                }
+            ]
         },
         {
-            title: "ReactJS / Redux"
+            title: "Engineering / Embedded Technologies",
+            skills: [
+                {
+                    title: "C++ / Arduino"
+                },
+                {
+                    title: "Python GPIO / PyQtGraph"
+                },
+                {
+                    title: "Linux"
+                }
+            ]
         },
         {
-            title: "Angular 7"
+            title: "Statistics",
+            skills: [
+                {
+                    title: "R"
+                },
+                {
+                    title: "Python Numpy / Pandas"
+                },
+                {
+                    title: "Applied Mathematics"
+                },
+                {
+                    title: "Excel"
+                }
+            ]
         },
         {
-            title: "Websockets / RESTful APIs"
-        },
-        {
-            title: "Data Visualisation / D3"
-        },
-        {
-            title: "C# / .NET"
-        },
-        {
-            title: "Python Flask / Django"
-        },
-        {
-            title: "linux"
-        },
-        {
-            title: "NGINX"
-        },
-    ]
-    const embedded = [
-        {
-            title: "C++ / Arduino"
-        },
-        {
-            title: "Python GPIO / PyQtGraph"
-        },
-        {
-            title: "Linux"
-        },
-    ]
-    const statistics = [
-        {
-            title: "R"
-        },
-        {
-            title: "Python Numpy / Pandas"
-        },
-        {
-            title: "Applied Mathematics"
-        },
-        {
-            title: "Excel"
-        }
-    ];
-    const laboratory = [
-        {
-            title: "Dissection"
-        },
-        {
-            title: "Biological Sample Husbandary"
-        },
-        {
-            title: "Parafin Wax Embedding"
-        },
-        {
-            title: "Microtome Sectioning"
-        },
-        {
-            title: "Histology / Immunohistochemistry"
-        },
-        {
-            title: "Microscopy (fluorescent)"
-        },
-        {
-            title: "Column Chromatography"
-        },
-        {
-            title: "Bioreactor Maintenance"
+            title: "Laboratory",
+            skills: [
+                {
+                    title: "Dissection"
+                },
+                {
+                    title: "Biological Sample Husbandary"
+                },
+                {
+                    title: "Parafin Wax Embedding"
+                },
+                {
+                    title: "Microtome Sectioning"
+                },
+                {
+                    title: "Histology / Immunohistochemistry"
+                },
+                {
+                    title: "Microscopy (fluorescent)"
+                },
+                {
+                    title: "Column Chromatography"
+                },
+                {
+                    title: "Bioreactor Maintenance"
+                }
+            ]
         }
     ];
     const { classes } = props;
     return (
         <div className={classes.root}>
-            <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>
-                        Web Technologies
-                    </Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <div>
-                        {webTechnologies.map(e => {
-                            return <div key={e.title}>{e.title}</div>;
-                        })}
-                    </div>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>
-                        Engineering / Embedded Technologies
-                    </Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <div>
-                        {embedded.map(e => {
-                            return <div key={e.title}>{e.title}</div>;
-                        })}
-                    </div>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>
-                        Statistics
-                    </Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <div>
-                        {statistics.map(e => {
-                            return <div key={e.title}>{e.title}</div>;
-                        })}
-                    </div>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>
-                        Scientific Laboratory
-                    </Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <div>
-                        {laboratory.map(e => {
-                            return <div key={e.title}>{e.title}</div>;
-                        })}
-                    </div>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
+            {skills.map(e => {
+                return (
+                    <ExpansionPanel key={e.title}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>
+                                {e.title}
+                            </Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <div>
+                                {e.skills.map(f => {
+                                    return <div key={f.title}>{f.title}</div>;
+                                })}
+                            </div>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                );
+            })}
         </div>
     );
 }
